ed0c0f5a3d37d11a0948d160338fff6acee7f2ca
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-07-14 13:04:13 +0100

    feat: add all visual structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/globals";
 import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+
+      <Transactions />
     </ThemeProvider>
   )
 }
